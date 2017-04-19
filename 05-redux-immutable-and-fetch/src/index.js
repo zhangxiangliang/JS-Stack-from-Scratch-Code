9ffa25802b19752e7442d10f77ed7fd4117889bb
zxl <326277403@qq.com>
2017-04-19 11:50:21 +0800

    test immutable fromJS and Map diff

--- 05-redux-immutable-and-fetch/src/index.js
+++ 05-redux-immutable-and-fetch/src/index.js
@@ -1,4 +1,14 @@
 var Immutable = require('Immutable')
 
-const obj = Immutable.Map({ a: 1 })
-console.log(obj.set('a', 2))
+const obj = Immutable.Map({
+    name: 'Stan',
+    friends: ['Kyle', 'Cartman', 'Kenny']
+})
+
+const immutablePerson = Immutable.fromJS({
+    name: 'Stan',
+    friends: ['Kyle', 'Cartman', 'Kenny']
+})
+
+console.log(obj);
+console.log(immutablePerson);
